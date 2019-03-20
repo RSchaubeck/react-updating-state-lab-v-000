ecc37deec9f4308c94662139c965a4ada9465bcd
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-20 19:54:07 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,9 +3,46 @@ import React from 'react';
 
 class YouTubeDebugger extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  }
+
+  handleResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
   render() {
     return(
-      ''
+      <button class='bitrate' onClick={this.handleBitrate}></button>
+      <button class='resolution' onClick={this.handleResolution}></button>
     )
   }
 }
+
+export default YouTubeDebugger;
